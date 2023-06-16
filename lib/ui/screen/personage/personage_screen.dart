80065127142b57f6248393edf27d8bcc71c80fad
IngNex <ingnexmaicol14@gmail.com>
2023-06-16 00:00:43 -0500

    Ui progress personageScreen

--- lib/ui/screen/personage/personage_screen.dart
+++ lib/ui/screen/personage/personage_screen.dart
@@ -10,7 +10,32 @@ class PersonageScreen extends StatefulWidget {
 class _PersonageScreenState extends State<PersonageScreen> {
   @override
   Widget build(BuildContext context) {
-    final name = widget.personageData.name;
-    return Scaffold(body: Center(child: Text(name)));
+    final personage = widget.personageData;
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Data Personage'),
+        ),
+        body: Card(
+          margin: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+          elevation: 5,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              20,
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.network(personage.image),
+              Text(
+                personage.name,
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              Text('Species: ${personage.species}'),
+            ],
+          ),
+        ));
   }
 }
